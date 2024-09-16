6c0874f44629f0444ae486dfd4c538133dae4fc1
Arya Segara <aryasegara10@gmail.com>
2024-09-16 08:48:15 +0700

    Day 1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,46 @@
 const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Menu</summary>
+          <ul className="p-2">
+            <li>
+              <a>All</a>
+            </li>
+            <li>
+              <a>Salad</a>
+            </li>
+            <li>
+              <a>Pizza</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Service</summary>
+          <ul className="p-2">
+            <li>
+              <a>Online Order</a>
+            </li>
+            <li>
+              <a>Table booking</a>
+            </li>
+            <li>
+              <a>Order Tracking</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Offers</a>
+      </li>
+    </>
+  );
   return (
     <header className="max-w-screen-2xl container mx-auto">
       <div className="navbar xl:px-20 ">
@@ -24,23 +66,7 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {navItems}
             </ul>
           </div>
           <a href="/">
@@ -49,27 +75,7 @@ const Navbar = () => {
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
