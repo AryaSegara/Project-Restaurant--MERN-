c7a67549994f6bfc5e64b18961c904e9f0004458
Arya Segara <aryasegara10@gmail.com>
2024-09-17 21:57:33 +0700

    Day Two

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,7 +1,84 @@
 export default function Banner() {
   return (
     <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8 bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-      <div className="py-20 flex flex-col md:flex-row justify-between items-center gap-8">
+      <div className="py-20 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+        {/* Image */}
+        <div className="md:w-1/2">
+          <img src="./images/bannerHome.png" alt="" />
+          <div className="flex flex-colo md:flex-row items-center justify-around -mt-14 gap-4">
+            <div className="flex bg-white py-2 px-3 rounded-2xl items-center gap-2 shadow-md w-64">
+              <img src="./images/banner-1.png" alt="" className="rounded-2xl" />
+              <div className="space-y-1">
+                <h5 className="text-sm mb-1">Spicy noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+                <p className="text-red">$18.00</p>
+              </div>
+            </div>
+            <div className="md:flex hidden bg-white py-2 px-3 rounded-2xl items-center gap-2 shadow-md w-64">
+              <img src="./images/banner-1.png" alt="" className="rounded-2xl" />
+              <div className="space-y-1">
+                <h5 className="text-sm mb-1">Spicy noodles</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-500"
+                  />
+                </div>
+                <p className="text-red">$18.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* Text */}
         <div className="md:w-1/2 space-y-7 px-4">
           <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
@@ -14,20 +91,6 @@ export default function Banner() {
               Order Now
             </button>
           </h2>
-        </div>
-
-        {/* Image */}
-        <div className="md:w-1/2">
-          <img src="./images/bannerHome.png" alt="" />
-          <div>
-            <div>
-              <img src="./images/banner-1.png" alt="" className="rounded-2xl" />
-              <div>
-                <h5>Spicy noodles</h5>
-                
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
