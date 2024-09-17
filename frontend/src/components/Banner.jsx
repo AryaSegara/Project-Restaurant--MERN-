4c5557b0244ef56711b9abefc33b349b1474356a
Arya Segara <aryasegara10@gmail.com>
2024-09-17 20:57:07 +0700

    Day Two

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,9 @@
 export default function Banner() {
   return (
     <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8 bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-      <div className="py-24 flex flex-col md:flex-row justify-between items-center gap-8">
+      <div className="py-20 flex flex-col md:flex-row justify-between items-center gap-8">
         {/* Text */}
-        <div className="md:w-1/2 space-y-7 px-6">
+        <div className="md:w-1/2 space-y-7 px-4">
           <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
             Dive into Delights of Delectable
             <span className="text-green"> Food</span>
@@ -16,9 +16,15 @@ export default function Banner() {
           </h2>
         </div>
 
-
-
-        <div className="md:w-1/2">Right</div>
+        {/* Image */}
+        <div className="md:w-1/2">
+          <img src="./images/bannerHome.png" alt="" />
+          <div>
+            <div>
+              <img src="./images/banner-1.png" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
