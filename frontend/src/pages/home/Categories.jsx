22926d7894a01e939f503b40b8c400a42554817b
Arya Segara <aryasegara10@gmail.com>
2024-09-19 16:10:39 +0700

    Day 4

---
+++ frontend/src/pages/home/Categories.jsx
@@ -0,0 +1,9 @@
+export default function Categories() {
+  return (
+    <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8 py-16">
+      <div>
+        <p className="text-red uppercase tracking-wide">Customer Favorites</p>
+      </div>
+    </div>
+  );
+}
