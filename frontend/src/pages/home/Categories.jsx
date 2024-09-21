8f696eb113bc9a4a32d10f42dac26bf275f43261
Arya Segara <aryasegara10@gmail.com>
2024-09-21 10:08:51 +0700

    Day 6

--- frontend/src/pages/home/Categories.jsx
+++ frontend/src/pages/home/Categories.jsx
@@ -1,3 +1,31 @@
+const categoryItems = [
+  {
+    id: 1,
+    title: "Main Dish",
+    des: "(86 dishes)",
+    image: "/images/home/category/img1.png",
+  },
+  {
+    id: 2,
+    title: "Break Fast",
+    des: "(12 Breakfast)",
+    image: "/images/home/category/img2.png",
+  },
+  {
+    id: 3,
+    title: "Dessert",
+    des: "(48 Desserts)",
+    image: "/images/home/category/img3.png",
+  },
+  {
+    id: 4,
+    title: "Browse All",
+    des: "(255 Items)",
+    image: "/images/home/category/img4.png",
+  },
+
+];
+
 export default function Categories() {
   return (
     <div className="max-w-screen-2xl container mx-auto lx:px-24 px-4 py-16">
@@ -6,10 +34,8 @@ export default function Categories() {
         <h2 className="title">Popular Categories</h2>
       </div>
 
-
       {/* Category Card */}
       <div></div>
-
     </div>
   );
 }
