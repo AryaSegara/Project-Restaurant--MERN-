cadb00214a2263ab2d63d7118396a48df88f7c39
Arya Segara <aryasegara10@gmail.com>
2024-09-23 16:25:06 +0700

    Day 8

---
+++ frontend/src/pages/home/SpecialDishes.jsx
@@ -0,0 +1,10 @@
+export default function SpecialDishes() {
+  return (
+    <div className="max-w-screen-2xl container mx-auto lx:px-24 px-4 my-20">
+      <div className="text-left">
+        <p className="subtitle">Special Dishes</p>
+        <h2 className="title">Standout Dishes From Our Menu</h2>
+      </div>
+    </div>
+  );
+}
