3a3db7d7661856ffa041239d7841f6690ee91ee2
Arya Segara <aryasegara10@gmail.com>
2024-09-23 16:27:16 +0700

    Day 8

--- frontend/src/pages/home/SpecialDishes.jsx
+++ frontend/src/pages/home/SpecialDishes.jsx
@@ -3,7 +3,7 @@ export default function SpecialDishes() {
     <div className="max-w-screen-2xl container mx-auto lx:px-24 px-4 my-20">
       <div className="text-left">
         <p className="subtitle">Special Dishes</p>
-        <h2 className="title">Standout Dishes From Our Menu</h2>
+        <h2 className="title md:w-[520px]">Standout Dishes From Our Menu</h2>
       </div>
     </div>
   );
