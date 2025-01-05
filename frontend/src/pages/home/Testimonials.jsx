2e88f86b0ea8dddc3d1803c9ab5c6827c9665cf5
Arya Segara <aryasegara10@gmail.com>
2025-01-05 13:39:55 +0700

    day 16

--- frontend/src/pages/home/Testimonials.jsx
+++ frontend/src/pages/home/Testimonials.jsx
@@ -1,10 +1,17 @@
-export default function Testimonials(){
-    return(
-        <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8">
-            <div className="flex flex-col">
-                <div className="md:w-1/2">Left</div>
-                <div className="md:w-1/2">Right</div>
-            </div>
+export default function Testimonials() {
+  return (
+    <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-12">
+        <div className="md:w-1/2">
+          <img src="/images/home/testimonials/testimonials.png" alt="" />
         </div>
-    )
+        <div className="md:w-1/2">
+          <div className="text-center">
+            <p className="subtitle">Customer Favorites</p>
+            <h2 className="title">Popular Categories</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
