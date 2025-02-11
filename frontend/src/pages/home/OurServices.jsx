56499bc9264f5261e5fd6785bbc4f8216e48b1c9
Arya Segara <aryasegara10@gmail.com>
2025-02-11 20:07:56 +0700

    first commit

--- frontend/src/pages/home/OurServices.jsx
+++ frontend/src/pages/home/OurServices.jsx
@@ -3,7 +3,9 @@ export default function OurServices() {
     <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8 my-20">
       <div className="flex flex-col md:flex-row items-center justify-between gap-12">
         {/* Text */}
+
         <div className="md:w-1/2">
+
           <div className="text-lef md:w-4/5">
             <p className="subtitle">Our Story & Services</p>
             <h2 className="title">Our Culinary Journey And Services</h2>
@@ -12,10 +14,11 @@ export default function OurServices() {
               offer exepetional services , bliding culinary artistry with warm
               hospitality
             </p>
-            
+
             <button className="btn bg-green text-white px-8 py-3 rounded-full">Explore</button>
 
           </div>
+          
         </div>
 
         
