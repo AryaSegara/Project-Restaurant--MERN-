854de52fb3a919e14d21a0aa7130e3317815b25b
Arya Segara <aryasegara10@gmail.com>
2025-05-09 20:41:12 +0700

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -38,6 +38,7 @@ export default function Footer() {
       <footer className="footer sm:footer-horizontal xl:px-10 py-10 px-4 items-center p-4">
         <aside className="grid-flow-col items-center">
           
+          
           <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
         </aside>
         <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
