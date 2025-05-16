82af5bcce47e3f617f2fb534f3d53cd0d77b8e0b
Arya Segara <aryasegara10@gmail.com>
2025-05-16 10:21:48 +0700

    menu

--- frontend/src/pages/shop/Menu.jsx
+++ frontend/src/pages/shop/Menu.jsx
@@ -3,7 +3,8 @@ const Menu = () => {
     <div>
       {/* Menu Banner */}
       <div className="max-w-screen-2xl container mx-auto lx:px-24 px-8 bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-        <div className="py-20 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+
+        <div className="py-20 flex flex-col justify-center items-center gap-8">
           {/* Text */}
           <div className="md:w-1/2 space-y-7 px-4">
             <h2 className="md:text-4xl text-3xl font-bold md:leading-snug leading-snug">
@@ -19,7 +20,7 @@ const Menu = () => {
             </h2>
           </div>
         </div>
-        
+
       </div>
     </div>
   );
