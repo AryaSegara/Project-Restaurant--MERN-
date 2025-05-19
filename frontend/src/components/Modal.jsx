2e7d44e1f5da75468fd83da144410b32efc8d5de
Arya Segara <aryasegara10@gmail.com>
2025-05-20 05:53:00 +0700

    modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,23 +1,28 @@
-const Modal = () =>{
-    return(
-        <dialog
-            id="my_modal_5"
-            className="modal modal-middle sm:modal-middle"
-          >
-            <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">
-                Press ESC key or click the button below to close
-              </p>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button in form, it will close the modal */}
-                  <button className="btn">Close</button>
-                </form>
+const Modal = () => {
+  return (
+    <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="py-4">Press ESC key or click the button below to close</p>
+        <div className="modal-action">
+
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
               </div>
-            </div>
-          </dialog>
-    )
-}
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
+          
+        </div>
+      </div>
+    </dialog>
+  );
+};
 
 export default Modal;
