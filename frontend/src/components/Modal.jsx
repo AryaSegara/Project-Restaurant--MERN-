98ee8d82e2fe1fec26f16c3490c57aaf2550ea14
Arya Segara <aryasegara10@gmail.com>
2025-05-20 06:11:30 +0700

    modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -2,22 +2,42 @@ const Modal = () => {
   return (
     <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
       <div className="modal-box">
-        
         <div className="modal-action mt-0">
-          <div className="card-body" method="dialog">
+          <form className="card-body" method="dialog">
             <h3 className="font-bold text-lg">Please Login!</h3>
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <input type="submit" value="login" className="btn btn-neutral mt-4" />
-            </fieldset>
-          </div>
 
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                className="input input-bordered"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                className="input input-bordered"
+                placeholder="Password"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+
+            <div className="form-control mt-6">
+                <input type="submit" value="login" className="btn bg-green text-white"/>
+            </div>
+          </form>
         </div>
       </div>
     </dialog>
