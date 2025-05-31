9eb3e2ff472cd6464e4906ae352afc4984953537
Arya Segara <aryasegara10@gmail.com>
2025-05-31 10:09:38 +0700

    signup

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -3,89 +3,90 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-    const {
+  const {
     register,
     handleSubmit,
-    formState : {errors},
-  } = useForm ()
+    formState: { errors },
+  } = useForm();
 
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="modal-action flex flex-col justify-center mt-0">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="card-body"
-        method="dialog"
-      >
-        <h3 className="font-bold text-lg">Please Login!</h3>
+    <div className="max-w-md">
+      <div className="modal-action flex flex-col justify-center mt-0">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="card-body"
+          method="dialog"
+        >
+          <h3 className="font-bold text-lg">Please Login!</h3>
 
-        {/* email */}
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="email"
-            className="input input-bordered"
-            placeholder="Email"
-            required
-            {...register("email")}
-          />
+          {/* email */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              className="input input-bordered"
+              placeholder="Email"
+              required
+              {...register("email")}
+            />
+          </div>
+
+          {/* password */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              className="input input-bordered"
+              placeholder="Password"
+              required
+              {...register("password")}
+            />
+
+            <label className="label mt-2">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+
+          {/* error */}
+
+          {/* Login btn */}
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              value="login"
+              className="btn bg-green text-white"
+            />
+          </div>
+
+          <p className="text-center my-2">
+            Dont gave an account?{" "}
+            <Link to="/signup" className="underline text-red ml-1">
+              {" "}
+              Signup Now
+            </Link>
+          </p>
+        </form>
+
+        {/* Social Sign in */}
+        <div className="text-center space-x-3 mb-5">
+          <button className="btn btn-circle hover:bg-green hover:text-white">
+            <FaGoogle />
+          </button>
+          <button className="btn btn-circle hover:bg-green hover:text-white">
+            <FaFacebookF />
+          </button>
+          <button className="btn btn-circle hover:bg-green hover:text-white">
+            <FaGithub />
+          </button>
         </div>
-
-        {/* password */}
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            className="input input-bordered"
-            placeholder="Password"
-            required
-            {...register("password")}
-          />
-          
-          <label className="label mt-2">
-            <a href="#" className="label-text-alt link link-hover">
-              Forgot password?
-            </a>
-          </label>
-        </div>
-
-        {/* error */}
-
-
-        {/* Login btn */}
-        <div className="form-control mt-6">
-          <input
-            type="submit"
-            value="login"
-            className="btn bg-green text-white"
-          />
-        </div>
-
-        <p className="text-center my-2">
-          Dont gave an account?{" "}
-          <Link to="/signup" className="underline text-red ml-1">
-            {" "}
-            Signup Now
-          </Link>
-        </p>
-      </form>
-
-      {/* Social Sign in */}
-      <div className="text-center space-x-3 mb-5">
-        <button className="btn btn-circle hover:bg-green hover:text-white">
-          <FaGoogle />
-        </button>
-        <button className="btn btn-circle hover:bg-green hover:text-white">
-          <FaFacebookF />
-        </button>
-        <button className="btn btn-circle hover:bg-green hover:text-white">
-          <FaGithub />
-        </button>
       </div>
     </div>
   );
