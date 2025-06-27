0129bd0be9d8553a5b56a96dc48fb23be4d7dd10
Arya Segara <aryasegara10@gmail.com>
2025-06-27 13:55:07 +0700

    authprovider

--- frontend/src/contexts/AuthProvider.jsx
+++ frontend/src/contexts/AuthProvider.jsx
@@ -1,21 +1,39 @@
 /* eslint-disable react/prop-types */
 import { Children, createContext, useState } from "react";
-
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = ({children}) =>{
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [user,setUser] = useState("Arya");
+  // create an account
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const authInfo ={
-        user
-    }
-    return(
-       <AuthContext.Provider value={authInfo}>
-        {Children}
-       </AuthContext.Provider>
-    )
-}
+  //   signup with email
+  const signUpWithGmail = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const authInfo = {
+    user,
+    createUser,
+    signUpWithGmail,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{Children}</AuthContext.Provider>
+  );
+};
 
 export default AuthProvider;
