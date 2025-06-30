2fd95e20b13226dbf1a79ea493316ac177f00947
Arya Segara <aryasegara10@gmail.com>
2025-06-30 13:48:08 +0700

    profile

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,15 +2,24 @@
 export default function Profile({ user }) {
   return (
     <div>
-      <div className="drawer drawer-end">
+      <div className="drawer drawer-end z-50">
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
           <label
             htmlFor="my-drawer-4"
-            className="drawer-button btn btn-primary"
+            className="drawer-button btn btn-ghost btn-circle avatar"
           >
-            Open drawer
+            <div className="w-10 rounded-full">
+              {user.photoURL ? (
+                <img alt="Tailwind CSS Navbar component" src={user.photoURL} />
+              ) : (
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              )}
+            </div>
           </label>
         </div>
         <div className="drawer-side">
@@ -22,10 +31,16 @@ export default function Profile({ user }) {
           <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
             {/* Sidebar content here */}
             <li>
-              <a>Sidebar Item 1</a>
+              <a>Profile</a>
             </li>
             <li>
-              <a>Sidebar Item 2</a>
+              <a>Order</a>
+            </li>
+            <li>
+              <a>Setting</a>
+            </li>
+            <li>
+              <a>Logout</a>
             </li>
           </ul>
         </div>
