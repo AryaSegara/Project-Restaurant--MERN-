9f926fdd55a694132676de5464240dd0cf1765ce
Arya Segara <aryasegara10@gmail.com>
2025-07-02 15:16:22 +0700

    main

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -2,18 +2,27 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import "../App.css";
 import Footer from "../components/Footer";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
 
 const Main = () => {
+  const { loading } = useContext(AuthContext);
+
   return (
     <div>
-      <Navbar />
-      <div className="min-h-screen">
-        <Outlet />
-      </div>
-      <Footer />
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <Navbar />
+          <div className="min-h-screen">
+            <Outlet />
+          </div>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
-
 
 export default Main;
