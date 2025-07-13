94e17d7b6e67eaab31f27be99b46cdcf444e9884
Arya Segara <aryasegara10@gmail.com>
2025-07-14 06:23:33 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-import 'dotenv/config'
+import "dotenv/config";
 // require('dotenv').config();
 // console.log(process.env);
 
@@ -13,14 +13,13 @@ const port = process.env.PORT || 6001;
 app.use(cors());
 app.use(express.json());
 
-
 // aryasegara353
 // Ra7yO7qqo4USftzh
 
 // mongodb config
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://aryasegara353:Ra7yO7qqo4USftzh@demo-foodie-cluster.3p5vya7.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodie-cluster";
+const uri = `mongodb+srv://aryasegara353:Ra7yO7qqo4USftzh@demo-foodie-cluster.3p5vya7.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodie-cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,7 +27,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -37,21 +36,18 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/', (req,res) =>{
-    res.send("Hello World");
-})
-
-app.listen(port,() =>{
-    console.log(`Aplikasi Berhasil jalan di port ${port}`);
-})
-
-
-
+app.listen(port, () => {
+  console.log(`Aplikasi Berhasil jalan di port ${port}`);
+});
